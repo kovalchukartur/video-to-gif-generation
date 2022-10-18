5a5e54a46861aa63e7be5f8f99ff984497cd0963
Artur Kovalchuk <arkovalchuk@playtika.com>
2022-10-18 12:07:01 +0300

    Polish

--- src/main/java/ua/kovalchuk/Runner.java
+++ src/main/java/ua/kovalchuk/Runner.java
@@ -1,8 +1,13 @@
 package ua.kovalchuk;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.Base64;
 import lombok.extern.slf4j.Slf4j;
 import org.bytedeco.javacpp.Loader;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -13,18 +18,27 @@ public class Runner implements CommandLineRunner {
     public void run(String... args) throws Exception {
         log.warn("Run runner");
 
+        String fileName = "input.mov";
+
+        ClassPathResource classPathResource = new ClassPathResource(fileName);
+        Path file = Files.createTempFile("", fileName);
+        Files.copy(classPathResource.getInputStream(), file, StandardCopyOption.REPLACE_EXISTING);
+        Path gifImageFile = Files.createTempFile(fileName, ".gif");
+
         String ffmpeg = Loader.load(org.bytedeco.ffmpeg.ffmpeg.class);
-        String inputVideoPath = "/Users/arkovalchuk/projects/ffmpeg-video-to-gif/src/main/resources/input.mov";
-        String outputVideoPath = "/Users/arkovalchuk/projects/ffmpeg-video-to-gif/src/main/resources/output.gif";
         ProcessBuilder pb = new ProcessBuilder(
             ffmpeg,
+            "-y", // Overwrite output files without asking. https://ffmpeg.org/ffmpeg.html#Main-options
             "-t", "5",
             "-filter_complex", "fps=5,scale=320:-1",
-            "-i", inputVideoPath,
-             outputVideoPath
+            "-i", file.toAbsolutePath().toString(),
+            gifImageFile.toAbsolutePath().toString()
         );
         pb.inheritIO().start().waitFor();
 
-        log.warn("Finish runner");
+        byte[] bytes = Files.newInputStream(gifImageFile).readAllBytes();
+        String base64String = Base64.getEncoder().encodeToString(bytes);
+        log.warn("{}", base64String);
+        log.info("Finish runner");
     }
 }
