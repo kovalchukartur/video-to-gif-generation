671bece9323fc4b5ff73b309c4fd89b71d0d6fe3
Artur Kovalchuk <arkovalchuk@playtika.com>
2022-10-18 10:30:30 +0300

    Init

---
+++ src/main/java/ua/kovalchuk/FfmpegVideoToGifApplication.java
@@ -0,0 +1,13 @@
+package ua.kovalchuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FfmpegVideoToGifApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FfmpegVideoToGifApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/kovalchuk/FfmpegVideoToGifApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.kovalchuk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FfmpegVideoToGifApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
