2da1750fc86a7ff4bbdbed3c141ccab99817d7e6
Artur Kovalchuk <arkovalchuk@playtika.com>
2022-10-18 11:17:40 +0300

    Add .gif generation

--- src/main/java/ua/kovalchuk/Runner.java
+++ src/main/java/ua/kovalchuk/Runner.java
@@ -1,6 +1,7 @@
 package ua.kovalchuk;
 
 import lombok.extern.slf4j.Slf4j;
+import org.bytedeco.javacpp.Loader;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,13 @@ public class Runner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         log.warn("Run runner");
+
+        String ffmpeg = Loader.load(org.bytedeco.ffmpeg.ffmpeg.class);
+        String inputVideoPath = "/Users/arkovalchuk/projects/ffmpeg-video-to-gif/src/main/resources/input.mov";
+        String outputVideoPath = "/Users/arkovalchuk/projects/ffmpeg-video-to-gif/src/main/resources/output.gif";
+        ProcessBuilder pb = new ProcessBuilder(ffmpeg, "-ss", "00:00:00", "-r", "5", "-i", inputVideoPath, "-f", "gif", outputVideoPath);
+        pb.inheritIO().start().waitFor();
+
         log.warn("Finish runner");
     }
 }
